a67cf90bd6fbdd566082ba2e4f06266ad36f65ae
anitha.konana <anitha.konana@gmail.com>
2023-12-08 11:53:35 +0530

    Improved code

--- Buy_computer.py
+++ Buy_computer.py
@@ -1,38 +1,34 @@
+available_parts = ["computer",
+                   "monitor",
+                   "keyboard",
+                   "mouse",
+                   "mouse mat",
+                   "hdmi cable",
+                   "dvd drive"
+                   ]
+current_choice = "-"
+computer_parts = [] # create an empty list
 
-current_choice = "_"
-available_parts = ["Computer",
-                   "Monitor",
-                   "Keyboard",
-                   "Mouse",
-                   "Mouse mat"]
-computer_parts  = []
-
-while current_choice != 0:
-    if current_choice in "12345":
+while current_choice != '0':
+    if current_choice in "123456":
         print("Adding {}".format(current_choice))
         if current_choice == '1':
-            computer_parts.append("Computer")
+            computer_parts.append("computer")
         elif current_choice == '2':
-            computer_parts.append("Monitor")
+            computer_parts.append("monitor")
         elif current_choice == '3':
-            computer_parts.append('Keyboard')
+            computer_parts.append("keyboard")
         elif current_choice == '4':
-            computer_parts.append('Mouse')
+            computer_parts.append("mouse")
         elif current_choice == '5':
-            computer_parts.append('Mouse_mat')
+            computer_parts.append("mouse mat")
+        elif current_choice == '6':
+            computer_parts.append("hdmi cable")
     else:
-        # print("Please select options from the list")
-        # print("1. Computer")
-        # print("2. Monitor")
-        # print("3. keyboard")
-        # print("4. mouse")
-        # print("5. mouse mat")
-        # print("0. Finish")
-        # for parts in available_parts:
-        #     print("{0}: {1}".format(current_choice)+1, parts)
-        for number,parts in enumerate(available_parts):
-            print("{0}: {1}".format(number+1, parts))
-
+        print("Please add options from the list below:")
+        for number, part in enumerate(available_parts):
+            print("{0}: {1}".format(number + 1, part))
 
     current_choice = input()
-print(current_choice, computer_parts)
+
+print(computer_parts)
